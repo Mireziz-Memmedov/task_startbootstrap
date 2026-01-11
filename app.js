6fbb329ef0f0ce6daf134d9581f5f0d45651f6d2
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-01-11 14:52:34 +0000

    commit

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.minimenu .btn').click(function (e) {
+        e.preventDefault();
+        $(this).next('.dropdown').slideToggle();
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.minimenu .btn').length && !$(e.target).closest('.dropdown').length) {
+            $('.dropdown').slideUp();
+        }
+    });
+
+    $('.minimenu .dropdown .option a').click(function (e) {
+        e.preventDefault();
+
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - $('header').outerHeight()
+        }, 500, function () {
+            $('.dropdown').slideUp();
+        });
+    });
+
 });
